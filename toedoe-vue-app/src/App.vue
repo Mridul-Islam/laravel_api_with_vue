d7849282f53cc215d57f791787dee48d9bf6fe73
Mridul-Islam <1610947@iub.edu.bd>
2023-08-27 06:32:36 +0600

    vue app created

---
+++ toedoe-vue-app/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import Navbar from './components/Navbar.vue';
+  import TaskPage from './pages/TasksPage.vue';
+  import Footer from './components/Footer.vue';
+</script>
+
+
+
+<template>
+  
+  <Navbar />
+  <TaskPage />
+  <Footer />
+
+</template>
+
+
+
+<style scoped>
+
+</style>
